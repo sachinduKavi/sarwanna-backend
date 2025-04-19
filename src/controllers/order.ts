6c73c28789d08c867a913889667a7c46ac774efb
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2025-04-20 01:31:06 +0530

    migrations

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -1,21 +1,17 @@
-import { Request, Response } from "express";
-import db from "../db/database";
-import {order} from "../db/schema";
+import {Request, Response} from "express";
 import {OrderService} from "../db/services/Order";
 
-const placeOrder=async (req:Request, res:Response) => {
+const placeOrder = async (req: Request, res: Response) => {
     let proceed = true, message = null, content = null
 
     try {
-        console.log("111111111111111")
-        await OrderService.placeOrder(req.body.customer,req.body.order,req.body.productList)
-
-    }catch(error){
-        console.log(error)
+        await OrderService.placeOrder(req.body.customer, req.body.order, req.body.productList)
+        message = "order Successfully saved"
+    } catch (error) {
         message = "order not places";
         proceed = false
     }
-    res.status(proceed?201:500).json({
+    res.status(proceed ? 201 : 500).json({
         proceed: proceed,
         message: message,
         content: content
